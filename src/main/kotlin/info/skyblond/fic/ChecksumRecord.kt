c79e2223f66363e48d8b24e3945cf70cd1420781
Rui Hu <hurui200320@skyblond.info>
2024-12-27 18:01:42 +0800

    Apply checks when using the compatibility mode

--- src/main/kotlin/info/skyblond/fic/ChecksumRecord.kt
+++ src/main/kotlin/info/skyblond/fic/ChecksumRecord.kt
@@ -14,8 +14,15 @@ data class ChecksumRecord(
                 // old format: hash size lastModify filename
                 // now we removed the lastModify,
                 // but still need to compatible with old fic file
-                val (hash, size, _, filename) = list
-                return ChecksumRecord(filename, size.toLong(), hash)
+                val timestamp = list[2].toLongOrNull() ?: 0L
+                if (timestamp > 1727740800) {
+                    val (hash, size, _, filename) = list
+                    return ChecksumRecord(filename, size.toLong(), hash)
+                } else {
+                    val (hash, size, filename1, filename2) = list
+                    return ChecksumRecord("$filename1 $filename2", size.toLong(), hash)
+
+                }
             } else {
                 val (hash, size, filename) = list
                 return ChecksumRecord(filename, size.toLong(), hash)
